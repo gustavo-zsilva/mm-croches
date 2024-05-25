47423c58a0cdd710893899ed30167e51b884b2f7
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-25 15:46:54 -0300

    added base color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        bg: '#FFFEF9',
+        black: '#070707',
+        primary: '#A0062C',
+      }
     },
   },
   plugins: [],
