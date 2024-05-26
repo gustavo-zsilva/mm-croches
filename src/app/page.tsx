6fb99d60661f735e3d668901da91a725151ba0dc
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-26 18:37:39 -0300

    added about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { About } from "@/components/About";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="min-h-screen space-y-10">
       <Header />
       <Main />
+      <About />
     </main>
   );
 }
