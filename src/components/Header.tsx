a248d3bc6118626be4ea01a08c2893fd9227672a
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-29 17:04:34 -0300

    completed nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,41 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Header() {
-    const pathname = usePathname()
+  const pathname = usePathname();
 
-    return (
-        <header className="flex flex-col items-center py-4 gap-4 px-6">
-            <Image
-                width={90}
-                height={90}
-                src="/images/logo.png"
-                alt="Logo"
-            />
-            <nav className="flex gap-8 flex-wrap">
-                <Link href="#" className={pathname === "/" ? 'font-bold underline underline-offset-8 decoration-2' : ''}>Home</Link>
-                <Link href="#">Sobre</Link>
-                <Link href="#">Loja</Link>
-                <button>Contato</button>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="flex flex-col items-center py-4 gap-4 px-6">
+      <Link href="/">
+        <Image width={90} height={90} src="/images/logo.png" alt="Logo" />
+      </Link>
+      <nav className="flex gap-8 flex-wrap">
+        <Link
+          href="/"
+          className={
+            pathname === "/"
+              ? "font-semibold underline underline-offset-8 decoration-2"
+              : ""
+          }
+        >
+          Home
+        </Link>
+        <Link href="#about">Sobre</Link>
+        <Link
+          href="/store"
+          className={
+            pathname === "/store"
+              ? "font-semibold underline underline-offset-8 decoration-2"
+              : ""
+          }
+        >
+          Loja
+        </Link>
+        <Link href="#contact">Contato</Link>
+      </nav>
+    </header>
+  );
 }
