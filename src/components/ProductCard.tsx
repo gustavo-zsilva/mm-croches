42d5f95dc0f986a70f7bf3641daa14a6cb6cabd8
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-29 17:04:19 -0300

    finished styling

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,31 +1,43 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { ArrowRight } from "lucide-react";
+import { Badge } from "./ui/badge";
 
 type ProductCardProps = {
-    item: {
-        imagePath: string,
-        name: string,
-        price: number,
-        description: string,
-    }
-}
+  item: {
+    imagePath: string;
+    name: string;
+    price: number;
+    description: string;
+    underMeasure: boolean;
+  };
+};
 
 export function ProductCard({ item }: ProductCardProps) {
-    return (
-        <li className="flex flex-col cursor-pointer rounded-lg w-full gap-4 hover:ring-4 hover:ring-gray-300 hover:ring-offset-8 transition-shadow">
-            <picture className="relative w-full h-36 rounded-lg overflow-hidden">
-                <Image
-                    src={`/images/products/${item.imagePath}.jpg`}
-                    fill
-                    sizes="(min-width: 800px) 50vw, 100vw"
-                    alt={item.name}
-                    className="object-cover"
-                />
-            </picture>
-            <div className="flex flex-col gap-1 font-medium">
-                <h3>{item.name}</h3>
-                <p className="text-black/60">{item.description}</p>
-                <span>{item.price}</span>
-            </div>
-        </li>
-    )
+  return (
+    <li className="flex flex-col cursor-pointer rounded-lg w-full gap-4 group">
+      <picture className="relative w-full h-36 rounded-lg overflow-hidden">
+        <Image
+          src={`/images/products/${item.imagePath}.jpg`}
+          fill
+          sizes="(min-width: 800px) 50vw, 100vw"
+          alt={item.name}
+          className="object-cover"
+        />
+      </picture>
+      <div className="flex flex-col gap-1 font-medium">
+        <header className="flex justify-between">
+          <h3>{item.name}</h3>
+          <ArrowRight
+            size={22}
+            className="group-hover:text-primary group-hover:-rotate-45 transition-transform"
+          />
+        </header>
+        <p className="text-black/60">{item.description}</p>
+        <footer className="flex items-center justify-between">
+          <span>{item.price}</span>
+          {item.underMeasure && <Badge>Sob medida</Badge>}
+        </footer>
+      </div>
+    </li>
+  );
 }
