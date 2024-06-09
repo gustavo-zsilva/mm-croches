a70fe9afb05f1a3737f2516f29af757dce606a79
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-09 20:19:39 -0300

    webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // This fixes Firebase Admin issue
+    webpack: (
+        config,
+    ) => {
+        
+        return config
+    },
+};
 
 export default nextConfig;
