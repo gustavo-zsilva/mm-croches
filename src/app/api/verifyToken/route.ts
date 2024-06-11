8e470095bd7f69a04dc7e641ba6197a73bfceb69
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-11 20:05:06 -0300

    created

---
+++ src/app/api/verifyToken/route.ts
@@ -0,0 +1,16 @@
+import admin from '@/lib/firebase/admin'
+import { type NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    const auth = admin.auth()
+    const idToken = request.cookies.get('token')?.value
+
+    try {
+        const decodedToken = await auth.verifyIdToken(idToken || '')
+        
+        return Response.json({ user: decodedToken.email, status: 201 })
+    } catch (err: any) {
+        return Response.json({ status: 500, ...err })
+    }
+
+}
