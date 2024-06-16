b4fd43b580b90f5c47064e98ca0bf7fb2631c24c
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-15 22:44:15 -0300

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { poppins } from "@/utils/fonts";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { LoggedInIndicator } from "@/components/LoggedInIndicator";
 
 export const metadata: Metadata = {
   title: "MM Cozy Crochet",
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <body className={`${poppins.className} m-auto`}>
-        {children}
+        <div className="relative">
+          {children}
+          <LoggedInIndicator />
+        </div>
         <Toaster />
       </body>
     </html>
