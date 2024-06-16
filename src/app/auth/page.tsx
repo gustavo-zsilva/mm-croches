67426111e71c26ed56f18edf396204c2d9993b46
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-15 22:45:12 -0300

    finished google login

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,22 +1,60 @@
 "use client"
 
+import { useRouter } from "next/navigation"
+
 import "@/lib/firebase/firebase"
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut, deleteUser } from "firebase/auth"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useToast } from "@/components/ui/use-toast"
 
 import { LoginForm } from "@/components/auth/LoginForm"
 import { RegisterForm } from "@/components/auth/RegisterForm"
 
 import { FcGoogle } from "react-icons/fc";
+import { handleSetToken } from "@/lib/actions/setToken"
 
 export default function Auth() {
     const auth = getAuth()
     const provider = new GoogleAuthProvider()
+    const router = useRouter()
+    const { toast } = useToast()
 
     async function handleSignInWithGoogle() {
         const result = await signInWithPopup(auth, provider)
-        console.log(result.user)
+        const user = result.user
+        
+        const whitelistedEmails = process.env.NEXT_PUBLIC_WHITELISTED_EMAILS?.split(',')
+
+        // FIX: Move check logic to server side
+        try {
+            if (!whitelistedEmails?.includes(user.email || "")) {
+                await deleteUser(user)
+                await signOut(auth)
+                return toast({
+                    title: 'Seu email não tem permissões de criação de conta!',
+                    variant: 'destructive',
+                })
+            }
+
+            toast({
+                title: 'Login realizado com sucesso!',
+                description: 'Redirecionando para a loja em 3 segundos...',
+            })
+
+            const idToken = await user.getIdToken()
+            handleSetToken({ idToken })
+
+            // Delay de 3 segundos
+            await new Promise(resolve => setTimeout(resolve, 3000))
+
+            router.push('/store/new-product')
+
+        } catch (err) {
+            console.error(err)
+            await deleteUser(user)
+            await signOut(auth)
+        }
     }
 
     return (
