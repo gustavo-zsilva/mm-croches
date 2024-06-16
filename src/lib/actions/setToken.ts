0ae5a8e01efffbdbc49d44fcbbeabd22f2040510
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-15 22:44:34 -0300

    created

---
+++ src/lib/actions/setToken.ts
@@ -0,0 +1,15 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+type ActionProps = {
+    idToken: string,
+}
+
+export async function handleSetToken({ idToken }: ActionProps) {
+    const cookieStore = cookies()
+    cookieStore.set('token', idToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+    })
+}
