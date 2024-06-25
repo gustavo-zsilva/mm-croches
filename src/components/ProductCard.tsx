c1edf259d3d953a70aadaf6f5a498a72e3f7540e
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-25 19:08:39 -0300

    molding to fit product type

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,11 +5,14 @@ import { CirclePlus } from "lucide-react";
 
 type ProductCardProps = {
   item: {
-    imagePath: string;
-    name: string;
-    price: number;
-    description: string;
-    underMeasure: boolean;
+    name: string,
+    description: string,
+    price: number,
+    type: string,
+    customMeasure: boolean,
+    prompDelivery: boolean,
+    images: string[],
+    id: string,
   },
   variant?: "default" | "store",
 };
@@ -19,6 +22,8 @@ export function ProductCard({ item, variant = "default" }: ProductCardProps) {
     default: "hover:ring hover:ring-offset-8 hover:ring-primary-light/80 transition-shadow",
     store: "",
   }
+
+  const formattedPrice = item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 
   return (
     <li className={`
@@ -33,7 +38,7 @@ export function ProductCard({ item, variant = "default" }: ProductCardProps) {
     `}>
       <picture className="relative w-full h-36 rounded-lg overflow-hidden">
         <Image
-          src={`/images/products/${item.imagePath}.jpg`}
+          src={item.images[0]}
           fill
           sizes="(min-width: 800px) 50vw, 100vw"
           alt={item.name}
@@ -50,8 +55,8 @@ export function ProductCard({ item, variant = "default" }: ProductCardProps) {
         </header>
         <p className="text-black/60">{item.description}</p>
         <footer className="flex items-center justify-between">
-          <span>R$ {item.price}</span>
-          {item.underMeasure && variant !== "store" && <Badge>Sob medida</Badge>}
+          <span>{formattedPrice}</span>
+          {item.customMeasure && variant !== "store" && <Badge>Sob medida</Badge>}
           {variant === "store" &&
             <button className="flex items-center gap-3 py-2 px-5 border-2 border-gray-300 rounded-2xl hover:border-teal-400 hover:bg-teal-50 transition-colors">
               <CirclePlus /> Carrinho
