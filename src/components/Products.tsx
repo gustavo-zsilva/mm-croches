6a1ba2ed02bdfcc724a191bc6a815824597060f8
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-28 19:27:25 -0300

    fixed types

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,31 +8,46 @@ import { query, limit, orderBy, collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/firebase/firebase";
 
 type Product = {
-  imagePath: string;
   name: string;
-  price: number;
   description: string;
-  underMeasure: boolean;
+  price: number;
+  type: string;
+  customMeasure: boolean;
+  promptDelivery: boolean;
+  images: string[];
+  id: string;
+};
+
+type DocData = {
+  name: string;
+  description: string;
+  price: number;
+  type: string;
+  customMeasure: boolean;
+  promptDelivery: boolean;
+  images: string[];
 };
 
 export async function Products() {
-  const productsRef = collection(db, "products")
-  const q = query(productsRef, orderBy("createdAt", "desc"), limit(3))
+  const productsRef = collection(db, "products");
+  const q = query(productsRef, orderBy("createdAt", "desc"), limit(3));
 
-  const querySnapshot = await getDocs(q)
-  const products: any[] = []
+  const querySnapshot = await getDocs(q);
+  const products: Product[] = [];
 
-  querySnapshot.forEach(doc => products.push(doc.data()))
-
-  console.log(products);
-  
+  querySnapshot.forEach((doc) =>
+    products.push({
+      ...(doc.data() as DocData),
+      id: doc.id,
+    }),
+  );
 
   return (
     <section className="flex flex-col items-center gap-10 max-w-screen-xl lg:m-auto lg:gap-20">
       <h1 className="text-primary">Peças recentes</h1>
       <ul className="flex flex-col w-full gap-6 md:grid md:grid-cols-3">
         {products.map((product) => (
-          <ProductCard key={product.name} item={product} />
+          <ProductCard key={product.id} item={product} />
         ))}
       </ul>
       <Link href="/store" className="w-full lg:flex lg:justify-center">
