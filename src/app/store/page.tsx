37271e4ccdb55981619c2fc6b4521492425306ef
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-28 19:25:33 -0300

    fixed types & added footer btn

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,55 +1,61 @@
+import Link from "next/link";
+
 import { Hero } from "@/components/Hero";
 import { ProductCard } from "@/components/ProductCard";
 import { StoreSection } from "@/components/StoreSection";
 import { StoreJumpCard } from "@/components/StoreJumpCard";
+import { Button } from "@/components/ui/button";
 
 import { collection, getDocs } from "firebase/firestore";
 
 import { db } from "@/lib/firebase/firebase";
+import { ArrowUpFromDot } from "lucide-react";
 
 type Product = {
-  name: string,
-  description: string,
-  price: number,
-  type: string,
-  customMeasure: boolean,
-  promptDelivery: boolean,
-  images: string[],
-  id: string,
-}
+  name: string;
+  description: string;
+  price: number;
+  type: string;
+  customMeasure: boolean;
+  promptDelivery: boolean;
+  images: string[];
+  id: string;
+};
 
 type DocData = {
-  name: string,
-  description: string,
-  price: number,
-  type: string,
-  customMeasure: boolean,
-  promptDelivery: boolean,
-  images: string[],
-}
+  name: string;
+  description: string;
+  price: number;
+  type: string;
+  customMeasure: boolean;
+  promptDelivery: boolean;
+  images: string[];
+};
 
 export default async function Store() {
-  const querySnapshot = await getDocs(collection(db, 'products'))
+  const querySnapshot = await getDocs(collection(db, "products"));
 
   // Get products
-  const docs: any[] = []
+  const docs: Product[] = [];
 
   querySnapshot.forEach((doc) => {
     docs.push({
-      ...doc.data(),
+      ...doc.data() as DocData,
       id: doc.id,
-    })
-  })
+    });
+  });
 
-  const amigurumiList = docs.filter(doc => doc.type === 'amigurumi')
-  const clothingList = docs.filter(doc => doc.type === 'clothing')
-  const accessoryList = docs.filter(doc => doc.type === 'accessory')
+  const amigurumiList = docs.filter((doc) => doc.type === "amigurumi");
+  const clothingList = docs.filter((doc) => doc.type === "clothing");
+  const accessoryList = docs.filter((doc) => doc.type === "accessory");
 
   return (
     <main className="min-h-screen flex flex-col gap-20 items-center">
       <Hero />
-      <section className="max-w-screen-xl w-full">
-        <h2 className="my-10 text-2xl text-center lg:text-left">O que te agrada? &gt;</h2>
+      <section className="max-w-screen-xl w-full" id="nav">
+        <h2 className="my-10 text-2xl text-center lg:text-left">
+          O que te agrada? &gt;
+        </h2>
         <ul className="flex flex-col gap-10 md:flex-row md:gap-5">
           <StoreJumpCard
             title="Amigurumis"
@@ -77,11 +83,7 @@ export default async function Store() {
         id="amigurumis"
       >
         {amigurumiList.map((doc) => (
-          <ProductCard
-            item={doc}
-            variant="store"
-            key={doc.id}
-          />
+          <ProductCard item={doc} variant="store" key={doc.id} />
         ))}
       </StoreSection>
       <StoreSection
@@ -90,11 +92,7 @@ export default async function Store() {
         id="clothing"
       >
         {clothingList.map((doc) => (
-          <ProductCard
-            item={doc}
-            variant="store"
-            key={doc.id}
-          />
+          <ProductCard item={doc} variant="store" key={doc.id} />
         ))}
       </StoreSection>
       <StoreSection
@@ -103,13 +101,20 @@ export default async function Store() {
         id="accessories"
       >
         {accessoryList.map((doc) => (
-          <ProductCard
-            item={doc}
-            variant="store"
-            key={doc.id}
-          />
+          <ProductCard item={doc} variant="store" key={doc.id} />
         ))}
       </StoreSection>
+      <section className="w-full max-w-screen-xl">
+        <Link href="/store/#nav">
+          <Button
+            variant="outline"
+            className="rounded-sm w-full py-6 text-gray-600"
+          >
+            Voltar ao topo
+            <ArrowUpFromDot size={20} />
+          </Button>
+        </Link>
+      </section>
     </main>
-  )
+  );
 }
