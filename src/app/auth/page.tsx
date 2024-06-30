1033a39b3f52c38258460563147944b7f97d8662
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-30 20:10:40 -0300

    added return link

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,19 +2,26 @@
 
 import "@/lib/firebase/firebase";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { LoginForm } from "@/components/auth/LoginForm";
 import { RegisterForm } from "@/components/auth/RegisterForm";
-
 import { GoogleLoginButton } from "@/components/auth/GoogleLoginButton";
+
+import { ArrowLeft } from "lucide-react";
 
 export default function Auth() {
   return (
     <main className="flex ">
       <div className="flex flex-col flex-1 p-9 gap-10 m-auto max-w-screen-sm">
-        <h1>Autenticação</h1>
+        <header className="flex items-center gap-6">
+          <Link href="/">
+            <ArrowLeft size={28} />
+          </Link>
+          <h1>Autenticação</h1>
+        </header>
         <GoogleLoginButton />
         <Tabs defaultValue="login">
           <TabsList className="grid w-full grid-cols-2">
