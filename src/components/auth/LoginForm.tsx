627fd101bed1179eaf2fbd9b5fec92d852a41628
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-30 19:29:24 -0300

    made submit button bigger

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,110 +1,122 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useToast } from "@/components/ui/use-toast"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
 
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth"
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 
-import { handleSetToken } from "@/lib/actions/setToken"
+import { handleSetToken } from "@/lib/actions/setToken";
 
 export const formSchema = z.object({
-    email: z.string().endsWith('@gmail.com', { message: 'Digite um email válido!' }),
-    password: z.string().min(1, { message: 'Você deve preencher este campo!' }),
-})
+  email: z
+    .string()
+    .endsWith("@gmail.com", { message: "Digite um email válido!" }),
+  password: z.string().min(1, { message: "Você deve preencher este campo!" }),
+});
 
 export function LoginForm() {
-    const { toast } = useToast()
-    const router = useRouter()
+  const { toast } = useToast();
+  const router = useRouter();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        }
-    })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    async function handleLoginUser({ email, password }: z.infer<typeof formSchema>) {
-        try {
-            const auth = getAuth()
-            const userCredential = await signInWithEmailAndPassword(auth, email, password)
-            
-            if (!userCredential.user.emailVerified) {
-                toast({
-                    title: 'Verifique o seu email antes de fazer login.',
-                    description: 'Enviamos um link ao seu email para confirmar que é realmente você.',
-                    variant: 'destructive',
-                })
-                await auth.signOut()
-                return
-            }
+  async function handleLoginUser({
+    email,
+    password,
+  }: z.infer<typeof formSchema>) {
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
 
-            const idToken = await userCredential.user.getIdToken()
-            
-            handleSetToken({ idToken })
-            
-            toast({
-                title: 'Login realizado com sucesso!',
-                description: 'Bem-vindo(a) de volta! Redirecionando para a loja em 3 segundos...',
-            })
-    
-            // Delay de 3 segundos
-            await new Promise(resolve => setTimeout(resolve, 3000))
+      if (!userCredential.user.emailVerified) {
+        toast({
+          title: "Verifique o seu email antes de fazer login.",
+          description:
+            "Enviamos um link ao seu email para confirmar que é realmente você.",
+          variant: "destructive",
+        });
+        await auth.signOut();
+        return;
+      }
 
-            router.push('/store/new-product')
-        } catch (err: any) {
-            toast({
-                title: 'Algo deu errado!',
-                description: err.message,
-                variant: 'destructive',
-            })
-        }
+      const idToken = await userCredential.user.getIdToken();
+      handleSetToken({ idToken });
+
+      toast({
+        title: "Login realizado com sucesso!",
+        description:
+          "Bem-vindo(a) de volta! Redirecionando para a loja em 3 segundos...",
+      });
+
+      // Delay de 3 segundos
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      router.push("/store/new-product");
+    } catch (err: any) {
+      toast({
+        title: "Algo deu errado!",
+        description: err.message,
+        variant: "destructive",
+      });
     }
+  }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleLoginUser)} className="space-y-8">
-                <FormField
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Email" type="email" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Senha</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Sua senha" type="password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" className="w-full rounded-sm">Entrar</Button>
-            </form>
-        </Form>
-    )
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleLoginUser)} className="space-y-8">
+        <FormField
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input placeholder="Sua senha" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full rounded-sm py-6">
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  );
 }
--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,107 +1,114 @@
-'use client'
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useToast } from "@/components/ui/use-toast"
-import { handleCreateUser } from "@/lib/actions/registerUser"
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { handleCreateUser } from "@/lib/actions/registerUser";
 
 export const formSchema = z.object({
-    email: z.string().email({ message: 'Digite um email válido!' }),
-    name: z.string(),
-    password: z.string().min(1, { message: 'Você deve preencher este campo!' }),
-})
+  email: z.string().email({ message: "Digite um email válido!" }),
+  name: z.string(),
+  password: z.string().min(1, { message: "Você deve preencher este campo!" }),
+});
 
 export function RegisterForm() {
-    const { toast } = useToast()
+  const { toast } = useToast();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            name: "",
-            password: "",
-        }
-    })
-    
-    async function handleRegisterUser({ name, email, password }: z.infer<typeof formSchema>) {
-        const response = await handleCreateUser({ name, email, password })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
 
-        if (response.status !== 200) {
-            return toast({
-                title: response.message,
-                variant: 'destructive',
-            })
-        }
+  async function handleRegisterUser({
+    name,
+    email,
+    password,
+  }: z.infer<typeof formSchema>) {
+    const response = await handleCreateUser({ name, email, password });
 
-        toast({
-            title: response.message,
-            description: 'Após isso, faça login na aba do lado!',
-        })
+    if (response.status !== 200) {
+      return toast({
+        title: response.message,
+        variant: "destructive",
+      });
     }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleRegisterUser)} className="space-y-8">
-                <FormField
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Nome</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Seu nome" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                Opcional
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Email" type="email" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                Email permitido pelo administrador para cadastro
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Senha</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Sua senha" type="password" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                Crie uma senha ou utilize a do seu email
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" className="w-full rounded-sm">Cadastrar</Button>
-            </form>
-        </Form>
-    )
+    toast({
+      title: response.message,
+      description: "Após isso, faça login na aba do lado!",
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleRegisterUser)}
+        className="space-y-8"
+      >
+        <FormField
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input placeholder="Seu nome" {...field} />
+              </FormControl>
+              <FormDescription>Opcional</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+              <FormDescription>
+                Email permitido pelo administrador para cadastro
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input placeholder="Sua senha" type="password" {...field} />
+              </FormControl>
+              <FormDescription>
+                Crie uma senha ou utilize a do seu email
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full rounded-sm py-6">
+          Cadastrar
+        </Button>
+      </form>
+    </Form>
+  );
 }
