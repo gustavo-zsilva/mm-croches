bed918f6f3316d7485aa38eb4165569afd8261ac
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-06-30 19:28:51 -0300

    prettier

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,11 +1,11 @@
-import { Poppins, Dancing_Script } from 'next/font/google'
+import { Poppins, Dancing_Script } from "next/font/google";
 
 export const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const dancingScript = Dancing_Script({
-    subsets: ["latin"],
-    weight: ["400", "700"],
-})
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
